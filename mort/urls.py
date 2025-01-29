9d38c41ae63d8dab6c04c37900d37e0e56626716
wesley <robinsonwesleyd@gmail.com>
2025-01-28 16:30:18 -0800

    put mortgage.urls behind /api

--- mort/urls.py
+++ mort/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("mortgage.urls")),
+    path("api/", include("mortgage.urls")),
     path("admin/", admin.site.urls),
 ]
