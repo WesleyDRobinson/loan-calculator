7672b10e34afb251438b83fc99c4498c3746e79f
wesley <robinsonwesleyd@gmail.com>
2025-01-30 02:16:20 -0800

    add UI routes; set base url '';

--- mortgage/urls.py
+++ mortgage/urls.py
@@ -1,10 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import LoanViewSet
+from .views import LoanViewSet, loan_list_view, loan_create_view, loan_detail_view
 
 router = DefaultRouter()
-router.register(r"loans", LoanViewSet)
+router.register(r"loans", LoanViewSet, basename="loan")
 
 urlpatterns = [
-    path("", include(router.urls)),
+    # Frontend Routes
+    path('', loan_list_view),
+    path('loans/', loan_list_view, name='loan-list'),
+    path('loans/create/', loan_create_view, name='loan-create-form'),
+    path('loans/<int:pk>/', loan_detail_view, name='loan-detail'),
+
+    # API Routes
+    path('', include(router.urls)),
 ]
