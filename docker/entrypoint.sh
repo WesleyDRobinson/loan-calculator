4c1286241c81dbb4ff5118eb957d7e5d72df4159
wesley <robinsonwesleyd@gmail.com>
2025-01-30 03:19:00 -0800

    narrowing in on actual dockerized

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,3 +5,6 @@ set -e
 echo "${0}: running migrationg"
 python manage.py makemigrations --merge
 python manage.py migrate --noinput
+
+echo "${0}: running server"
+exec python manage.py runserver 0.0.0.0:8000
